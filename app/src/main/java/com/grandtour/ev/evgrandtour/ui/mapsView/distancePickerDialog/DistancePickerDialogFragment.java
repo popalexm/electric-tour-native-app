9a4c8a13f36d9400b3775b4ef12608afbf1bc589
AlexPop <popalex.m@gmail.com>
2018-10-31 17:09:39 +0200

    Fixed dialog layout + added rounded dialog for both of them.

--- app/src/main/java/com/grandtour/ev/evgrandtour/ui/mapsView/distancePickerDialog/DistancePickerDialogFragment.java
+++ app/src/main/java/com/grandtour/ev/evgrandtour/ui/mapsView/distancePickerDialog/DistancePickerDialogFragment.java
@@ -6,12 +6,15 @@ import com.grandtour.ev.evgrandtour.databinding.FragmentDistancePickerBinding;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Toast;
 
 import java.util.Collection;
@@ -34,8 +37,13 @@ public class DistancePickerDialogFragment extends DialogFragment implements Dist
         FragmentDistancePickerBinding binding = DataBindingUtil.inflate(LayoutInflater.from(getContext()), R.layout.fragment_distance_picker, null, false);
         binding.setViewModel(distancePickerViewModel);
         binding.setPresenter(presenter);
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        }
         return binding.getRoot();
     }
+
 
     @Override
     public void onAttach(Context context) {
