79010e05ef1a4c736a27be382432bb55f527f872
popal <popalex.m@gmail.com>
2018-12-27 00:18:58 +0200

    Modified YAxisValueFormatter to display all meter values above 1000 as Kilometers

--- app/src/main/java/com/grandtour/ev/evgrandtour/ui/mainMapsView/chartViewFormaters/YAxisValueFormatter.java
+++ app/src/main/java/com/grandtour/ev/evgrandtour/ui/mainMapsView/chartViewFormaters/YAxisValueFormatter.java
@@ -11,9 +11,15 @@ public class YAxisValueFormatter implements IAxisValueFormatter {
 
     @NonNull
     private final DecimalFormat mFormat = new DecimalFormat("###,###,###");
+    private final static float KILOMETER_VALUE = 1000;
 
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
-        return mFormat.format(value) + " m";
+        if (value >= YAxisValueFormatter.KILOMETER_VALUE) {
+            value = value / YAxisValueFormatter.KILOMETER_VALUE;
+            return mFormat.format(value) + " Km";
+        } else {
+            return mFormat.format(value) + " m";
+        }
     }
 }
